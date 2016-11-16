8684ed2ff250efdf22a4764257385528367dca0b
Luke Edwards <luke.edwards05@gmail.com>
2016-11-15 19:31:44 -0800

    update 'elastic' name

--- example/index.js
+++ example/index.js
@@ -46,7 +46,7 @@ class App extends Component {
       tickSize: 3,
       mouseover: tip.show,
       mouseout: tip.hide,
-      ease: 'elastic',
+      ease: 'easeElastic',
       color: ['RGB(0, 177, 240)', 'rgb(243, 43, 101)']
     })
 
